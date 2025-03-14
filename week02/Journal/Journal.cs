8757a4ae8dcb86413f4726af16e743a541af4d03
HarryPotter5972 <brandonbgamer@gmail.com>
2025-03-13 19:10:30 -0600

    Created Entry.cs, Journal.cs, and PromptGenerator.cs

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Journal
+{
+    public List<Entry> _journalEntries = new List<Entry>();
+    
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+
+    public void DisplayAll()
+    {
+
+    }
+
+    public void SaveToFIle(string file)
+    {
+
+    }
+    
+    public void LoadFromFile(string file)
+    {
+
+    }
+
+
+}
