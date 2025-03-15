f9dfd49f86231472d045f364c6a0ebe8880c6aca
HarryPotter5972 <brandonbgamer@gmail.com>
2025-03-15 02:11:51 -0600

    edited Journal.cs

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -9,8 +9,14 @@ class Journal
         PromptGenerator promptText = new PromptGenerator();
         string prompt1 = "What did you do today?";
         string prompt2 = "What are you planning to do tomorrow?";
+        string prompt3 = "Did you do your scripture study today? If so, what did you study?";
+        string prompt4 = "Did you do your course work today? If so, for what class?";
+        string prompt5 = "Did you play video games today? If so, did you play on console or PC and which game or games did you play?";
         promptText._prompt.Add(prompt1);
         promptText._prompt.Add(prompt2);
+        promptText._prompt.Add(prompt3);
+        promptText._prompt.Add(prompt4);
+        promptText._prompt.Add(prompt5);
         DateTime today = DateTime.Now;
         newEntry._date = today.ToShortDateString();           
         newEntry._promptText = promptText.GetRandomPrompt(promptText._prompt);
